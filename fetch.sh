13ab2000d0e06c783e568ad194df6c4c4d52c54f
weiguo.ni <neefrankie@gmail.com>
2016-06-03 16:04:00 +0800

    use wget to fetch orginal data from natural earth

--- fetch.sh
+++ fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# wget --directory-prefix=.tmp --timestamping --input-file=natural_earth_urls.txt
+wget --directory-prefix=.tmp --timestamping --input-file=natural_earth_urls.txt
 for f in .tmp/*.zip
 do
 	echo "$f"
 	echo "${f%*.zip}"	
-	# unzip -d "${f%*.zip}" "$f"
+	unzip -d "${f%*.zip}" "$f"
 done
